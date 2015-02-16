54526a7590a7b3137b840c6f0723d8c62a610dea
Olivia Whitman <whitman.68@osu.edu>
2015-02-16 17:32:02 -0500

    Added reconstruct_path function and script. Updated the BFS script to include description of file at the top.

--- BFS.m
+++ BFS.m
@@ -1,5 +1,11 @@
-% function BFS(G, x):
-%   x.visited = true
+% Breadth First Search algorithm for solving an 8-puzzle.
+% Attempts to find the goal state [123456789].
+% -- arguments -- 
+% start_node: the initial state of the 8-puzzle.
+% graph:
+function BFS = BFS(graph, start_node)
+    start_node.visited = true;
+    start_node.parent = null;
 %   queue q = x
 %   tree T = x
 %   current_vertex = x
@@ -15,5 +21,6 @@
 %                 if current_vertex = goal
 %                       reconstruct_path(T, current_vertex)
 %                       return
+end
                       
                      
---
+++ reconstruct_path.m
@@ -0,0 +1,10 @@
+% Reconstruct the path from the given node to the root of the tree
+% -- arguments --
+% tree: the tree with all searched nodes
+% node: the current node in the tree 
+function reconstruct_path = reconstruct_path(tree, node)
+% total_path = current
+% while current in tree
+%   current = current.parent
+%   total_path.append(current)
+% print total_path
