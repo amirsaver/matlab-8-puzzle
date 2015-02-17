fdc5b57d82acd1eaa7ac38b441e658ede9587743
Jae Lee <icarurs@gmail.com>
2015-02-16 20:50:40 -0500

    Finish script for generating puzzles

--- puzzleCreation.m
+++ puzzleCreation.m
@@ -1,10 +1,25 @@
 % Puzzle Creation generates 4 puzzles for each depth starting at depth 5
 % and continuing down to depth 30 for a total of 100 puzzles.
 
-count = 0
+puzzles = []
+
 for n = 5:29
-    for m = 1:4
-        pgenerator(n)
-        count = count + 1
-    end
+    numPuzzleForEachDepth = 0;
+    
+    while(numPuzzleForEachDepth < 4)
+        array = pgenerator(n)
+
+        alreadyCreated = false;
+        for i = 1:size(puzzles, 1)
+            if(isequal(puzzles(i,:), array))
+                alreadyCreated = true;
+                break;
+            end
+        end
+        
+        if(alreadyCreated == false)
+            puzzles = vertcat(puzzles, array);
+            numPuzzleForEachDepth = numPuzzleForEachDepth + 1;
+        end
+    end % while
 end
