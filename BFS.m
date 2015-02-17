6d653d0b12b7f0ba7c6706a6156dadd54346118c
Olivia Whitman <whitman.68@osu.edu>
2015-02-17 14:24:10 -0500

    Removed excess print statements and debug lines from BFS

--- BFS.m
+++ BFS.m
@@ -2,7 +2,7 @@
 % Attempts to find the goal state [123456789].
 % -- arguments -- 
 % start_node: the initial state of the 8-puzzle.
-function result = BFS(start_node)
+function BFS(start_node)
 tic %start timer
 goal = 1:9;
 % starting node is the initial state, it has no parent since it is the
@@ -24,17 +24,14 @@ while ~isempty(queue)
     visited_index = visited_index + 1;
     
     if(isequal(node.state,goal))
-        disp(reconstruct_path(node))
+        reconstruct_path(node);
         toc % display time elapsed
-        result = true; %%%% DEBUG %%%%
         return
     else
-        toc %%%% DEBUG %%%%
         %for each possible move add to the queue if it's state has not been
         %visited
         nodeMoveLeft = moveBlankLeft(node);
         if(~ismember(nodeMoveLeft.state, visited, 'rows'))
-            toc
             nodeMoveLeft.parent = node;
             queue(length(queue) + 1) = nodeMoveLeft;
         end
@@ -56,13 +53,10 @@ while ~isempty(queue)
             nodeMoveUp.parent = node;
             queue(length(queue) + 1) = nodeMoveUp;
         end
-        toc %%%% DEBUG %%%%
-        disp(queue) %%%% DEBUG %%%%
     end % if end
 end % while end
-result = false; %%%% DEBUG -- failed BFS if returns 0 %%%%
 return 
-end % function ed
+end % function end
 
                       
                      
