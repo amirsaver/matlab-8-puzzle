8fd024dab43d47d43ca4897d2bfefdbb6969412f
Olivia Whitman <whitman.68@osu.edu>
2015-02-19 14:08:32 -0500

    Moved over remainder pgenerator2 to pgenerator

--- pgenerator.m
+++ pgenerator.m
@@ -8,78 +8,74 @@
 % size(A, 1) returns number of columns
 
 function pgenerator = pgenerator(max_depth)
-    in_order = int8([1, 2, 3, 4, 5, 6, 7, 8, 9]);
+    in_order = 123456789;
 
-    visited = int8([1, 2, 3, 4, 5, 6, 7, 8, 9]);
+    visited = in_order;
 
     depth = 0;
-    p1 = puzzle(in_order);
+    p1 = puzzle2(in_order);
 
     while depth < max_depth
         movement = randi(4);
 
         if(movement == 1)
-            temp = p1;
             p1 = moveBlankUp(p1);
 
             alreadyVisited = false;
             for i = 1:size(visited, 1)
-                if(isequal(visited(i,:), p1.state))
+                if(visited(i) == p1.state)
                     alreadyVisited = true;
                 end
             end
 
             if(alreadyVisited ~= true)
-                visited = vertcat(visited, p1.state);
+                visited(length(visited) + 1) = p1.state;
                 depth = depth + 1;
             end
         elseif(movement == 2)
-            temp = p1;
             p1 = moveBlankDown(p1);
 
             alreadyVisited = false;
             for i = 1:size(visited, 1)
-                if(isequal(visited(i,:), p1.state))
+                if(visited(i) == p1.state)
                     alreadyVisited = true;
                 end
             end
 
             if(alreadyVisited ~= true)
-                visited = vertcat(visited, p1.state);
+                visited(length(visited) + 1) = p1.state;
                 depth = depth + 1;
             end
         elseif(movement == 3)
-            temp = p1;
             p1 = moveBlankLeft(p1);
 
             alreadyVisited = false;
             for i = 1:size(visited, 1)
-                if(isequal(visited(i,:), p1.state))
+                if(visited(i) == p1.state)
                     alreadyVisited = true;
                 end
             end
 
             if(alreadyVisited ~= true)
-                visited = vertcat(visited, p1.state);
+                visited(length(visited) + 1) = p1.state;
                 depth = depth + 1;
             end
         else
-            temp = p1;
             p1 = moveBlankRight(p1);
 
             alreadyVisited = false;
             for i = 1:size(visited, 1)
-                if(isequal(visited(i,:), p1.state))
+                if(visited(i) == p1.state)
                     alreadyVisited = true;
                 end
             end
 
             if(alreadyVisited ~= true)
-                visited = vertcat(visited, p1.state);
+                visited(length(visited) + 1) = p1.state;
                 depth = depth + 1;
             end
         end
     end %while end  
 
-    pgenerator = int8(p1.state);
+    pgenerator = p1.state;
 end %function end
